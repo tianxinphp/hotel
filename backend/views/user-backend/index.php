e3c5ad57c9ac7ed307327bd4b0cb419b11ae8465
tianxin <844577216@qq.com>
2019-10-17 17:20:18 +0800

    user

--- backend/views/user-backend/index.php
+++ backend/views/user-backend/index.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
     </p>
 
     <?php Pjax::begin(); ?>
-    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+<!--    --><?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
