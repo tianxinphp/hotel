80e7a67968612389328bd37501824ba9653507ad
tianxin <844577216@qq.com>
2019-11-21 14:46:29 +0800

    菜单

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -1,81 +1,16 @@
+<?php
+
+use mdm\admin\components\MenuHelper;
+use dmstr\widgets\Menu;
+?>
+
 <aside class="main-sidebar">
-
     <section class="sidebar">
-
-        <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
-            </div>
-            <div class="pull-left info">
-                <p>Alexander Pierce</p>
-
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
-
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
-        <!-- /.search form -->
-
-        <?= dmstr\widgets\Menu::widget(
-            [
-                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
-                'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' =>'User Backend','icon'=>'user','url'=>['/user-backend']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                      'label'=>'权限',
-                      'icon'=>'list',
-                      'url'=>'#',
-                      'items'=>[
-                          ['label'=>'路由','icon'=>'list-ul','url'=>['/admin/route']],
-                          ['label'=>'权限菜单','icon'=>'list-ul','url'=>['/admin/permission']],
-                          ['label'=>'角色','icon'=>'list-ul','url'=>['/admin/role']],
-                          ['label'=>'角色分配','icon'=>'list-ul','url'=>['/admin/assignment']],
-                      ]
-                    ],
-                    [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ]
-        ) ?>
-
+        <?=
+        Menu::widget([
+            'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+            'items' => MenuHelper::getAssignedMenu(Yii::$app->user->id)
+        ]);
+        ?>
     </section>
-
 </aside>
