fde2212ff11dea72e944c1b124b17abd1fb52906
tianxin <844577216@qq.com>
2019-11-21 15:05:55 +0800

    菜单

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -7,7 +7,6 @@ use dmstr\widgets\Menu;
 <aside class="main-sidebar">
     <section class="sidebar">
         <?=
-        print_r(MenuHelper::getAssignedMenu(Yii::$app->user->id));
         Menu::widget([
             'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
             'items' => MenuHelper::getAssignedMenu(Yii::$app->user->id)
