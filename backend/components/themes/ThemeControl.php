352ec26f8a3d912ca887c8ae685aba8e0433844c
tianxin <844577216@qq.com>
2019-12-06 11:34:48 +0800

    主题

--- backend/components/themes/ThemeControl.php
+++ backend/components/themes/ThemeControl.php
@@ -14,6 +14,8 @@ class ThemeControl extends ActionFilter
 {
     public function init()
     {
+        echo 1;
+        exit();
         print_r(Yii::getAlias('@app'));
         exit();
         $switch=intval(Yii::$app->request->get('switch'));
