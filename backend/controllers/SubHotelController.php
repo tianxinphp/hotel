bdb695c1a3dadc45179178cd5e974e7cb3464833
tianxin <844577216@qq.com>
2019-12-12 11:32:13 +0800

    连表

--- backend/controllers/SubHotelController.php
+++ backend/controllers/SubHotelController.php
@@ -125,7 +125,7 @@ class SubHotelController extends Controller
                 $transaction->rollBack();
                 throw $e;
             }
-            return $this->redirect(['index']);
+//            return $this->redirect(['index']);
         }else{
             $model->tag=HotelTag::getTagBySubHotel($id);
             return $this->render('update', [
